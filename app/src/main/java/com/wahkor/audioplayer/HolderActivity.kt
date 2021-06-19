9dd5a6f9bf21c55a3ba33399a602e7290e94da5d
iryu <namboonma@gmail.com>
2021-06-19 17:20:39 +0700

    change to use viewBinding

--- app/src/main/java/com/wahkor/audioplayer/HolderActivity.kt
+++ app/src/main/java/com/wahkor/audioplayer/HolderActivity.kt
@@ -6,22 +6,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.wahkor.audioplayer.databinding.ActivityHolderBinding
+import com.wahkor.audioplayer.fragment.MenuFragment
+import com.wahkor.audioplayer.service.AudioService
 
 class HolderActivity : AppCompatActivity() {
+    private val binding:ActivityHolderBinding by lazy { ActivityHolderBinding.inflate(layoutInflater) }
+    private val audioService=AudioService()
     private lateinit var viewModel: PlayerViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_holder)
-
+        setContentView(binding.root)
 
         viewModel= ViewModelProvider.AndroidViewModelFactory(Application()).create(PlayerViewModel::class.java)
+        audioService.getPlayerInfo.observe(this,{
+            binding.holderTitle.text=it.tableName
+        })
+        binding.holderSetting.setOnClickListener {
+            val intent=Intent(this,PlayerActivity::class.java);startActivity(intent)
+        }
         //viewModel.retrievePlaylist(this)
 
-       /* supportFragmentManager.beginTransaction().replace(R.id.holderFragment,
+        supportFragmentManager.beginTransaction().replace(R.id.holderFragment,
             PlayerFragment.newInstance(),"Player").commit()
-        holderMenu.setOnClickListener {
-            toast("this is menu")
-        }*/
+            var i=0
+        binding.holderMenu.setOnClickListener {
+            if (i==0){
+                i=1
+                supportFragmentManager.beginTransaction().add(R.id.holderFragment,
+                    MenuFragment.newInstance(),"Menu").commit()
+                toast("open menu")
+            }else{
+                i=0
+                supportFragmentManager.beginTransaction().remove(
+                    PlayerFragment.newInstance()).commit()
+                toast("close menu")
+            }
+        }
+
     }
 
     private fun toast(s: Any) {
